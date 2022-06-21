9d871fb38e1ce7c7807186da26599978ce78d229
Aaron Rohrbacher <arjazz@icloud.com>
2022-06-20 22:29:49 -0700

    fixes wp-cli if statement

--- install.sh
+++ install.sh
@@ -1,2 +1,2 @@
 # Checks for WP CLI prerequisite, installs if not met.
-[ -f ~/public_html/wp-cli.phar ] && echo "wp-cli installed properly, continuing..." || echo "wp-cli is not installed, or is installed improperly. Installing now..."; cd ~/public_html; curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar; alias wp='~/public_html/wp-cli.phar';
+[ -f ~/public_html/wp-cli.phar ] && echo "wp-cli installed properly, continuing..." || { echo "wp-cli is not installed, or is installed improperly. Installing now..."; cd ~/public_html; curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar; alias wp='~/public_html/wp-cli.phar'; }
