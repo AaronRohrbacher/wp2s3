96a2bae45e638de3bc58940755b8ad84b9898eae
Aaron Rohrbacher <arjazz@icloud.com>
2022-06-20 23:10:43 -0700

    check for AWS CLI installation again3

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #Checks for AWS CLI installation (required)
 aws -h
-[$? == 0] && echo "AWS CLI is installed properly, continuing..." || {echo "AWS CLI is not installed, or you don't have access to it. Visit https://nuel.io/ for better hosting."; exit N;}
+[$? == 0] && echo "AWS CLI is installed properly, continuing..." || {echo "AWS CLI is not installed, or you don't have access to it. Visit https://nuel.io/ for better hosting."; exit 0;}
 # Checks for WP CLI prerequisite, installs if not met.
 [ -f ~/public_html/wp-cli.phar ] && echo "wp-cli installed properly, continuing..." || { echo "wp-cli is not installed, or is installed improperly. Installing now..."; cd ~/public_html; curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar; alias wp='~/public_html/wp-cli.phar'; }
